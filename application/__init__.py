29451c6e7ead3447b84f30b64e7594500f50a8c9
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-08-07 19:24:48 +0300

    configure heroku to use psql

--- application/__init__.py
+++ application/__init__.py
@@ -2,8 +2,14 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///events.db"
-app.config["SQLALCHEMY_ECHO"] = True
+
+import os
+
+if os.environ.get("HEROKU"):
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///events.db"
+    app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
@@ -21,6 +27,7 @@ from os import urandom
 app.config["SECRET_KEY"] = urandom(32)
 
 from flask_login import LoginManager
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -31,4 +38,7 @@ login_manager.login_message = "Please login to use this functionality."
 def load_user(user_id):
     return User.query.get(user_id)
 
-db.create_all()
+try:
+    db.create_all()
+except:
+    pass
