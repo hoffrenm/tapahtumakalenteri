3f12a58fbfffcf25274f37881571982b916c44a1
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-08-21 22:51:52 +0300

    start working on custom login wrapper for roles

--- application/__init__.py
+++ application/__init__.py
@@ -13,17 +13,6 @@ else:
 
 db = SQLAlchemy(app)
 
-from application import views
-
-from application.events import models
-from application.events import views
-
-from application.auth import models 
-from application.auth import views
-
-from application.comments import models
-
-from application.auth.models import User
 from os import urandom
 
 app.config["SECRET_KEY"] = urandom(32)
@@ -36,6 +25,48 @@ login_manager.init_app(app)
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
+# roles in login_required
+from functools import wraps
+
+def login_required(role="ANY"):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_view(*args, **kwargs):
+            if not current_user:
+                return login_manager.unauthorized()
+
+            if not current_user.is_authenticated:
+                return login_manager.unauthorized()
+            
+            unauthorized = False
+
+            if role != "ANY":
+                unauthorized = True
+                
+                for user_role in current_user.roles():
+                    if user_role == role:
+                        unauthorized = False
+                        break
+
+            if unauthorized:
+                return login_manager.unauthorized()
+            
+            return fn(*args, **kwargs)
+        return decorated_view
+    return wrapper
+
+from application import views
+
+from application.events import models
+from application.events import views
+
+from application.auth import models 
+from application.auth import views
+
+from application.comments import models
+
+from application.auth.models import User
+    
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
