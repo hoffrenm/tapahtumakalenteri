fb2e1df4899016449af986bce80a0f849e5ad69e
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-08-23 22:31:00 +0300

    configure flask-security

--- application/__init__.py
+++ application/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_security import Security, SQLAlchemyUserDatastore
 
 import os
 
@@ -17,43 +18,13 @@ from os import urandom
 
 app.config["SECRET_KEY"] = urandom(32)
 
-from flask_login import LoginManager
+from flask_login import LoginManager, current_user
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
-
-# roles in login_required
-from functools import wraps
-
-def login_required(role="ANY"):
-    def wrapper(fn):
-        @wraps(fn)
-        def decorated_view(*args, **kwargs):
-            if not current_user:
-                return login_manager.unauthorized()
-
-            if not current_user.is_authenticated:
-                return login_manager.unauthorized()
-            
-            unauthorized = False
-
-            if role != "ANY":
-                unauthorized = True
-                
-                for user_role in current_user.roles():
-                    if user_role == role:
-                        unauthorized = False
-                        break
-
-            if unauthorized:
-                return login_manager.unauthorized()
-            
-            return fn(*args, **kwargs)
-        return decorated_view
-    return wrapper
 
 from application import views
 
@@ -65,7 +36,10 @@ from application.auth import views
 
 from application.comments import models
 
-from application.auth.models import User
+from application.auth.models import User, Role
+
+user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
     
 @login_manager.user_loader
 def load_user(user_id):
@@ -73,5 +47,8 @@ def load_user(user_id):
 
 try:
     db.create_all()
+    user_datastore.find_or_create_role('admin')
+    user_datastore.find_or_create_role('enduser')
+    user_datastore.commit()
 except:
     pass
