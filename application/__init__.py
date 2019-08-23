4ffacf100e6344c2355e64cd9b3dab50fdfbfb33
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-08-24 00:32:20 +0300

    finalize flask security

--- application/__init__.py
+++ application/__init__.py
@@ -26,29 +26,24 @@ login_manager.init_app(app)
 login_manager.login_view = "auth_login"
 login_manager.login_message = "Please login to use this functionality."
 
-from application import views
-
-from application.events import models
-from application.events import views
-
 from application.auth import models 
-from application.auth import views
-
 from application.comments import models
+from application.events import models
 
 from application.auth.models import User, Role
 
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
 security = Security(app, user_datastore)
-    
+
+from application import views
+from application.auth import views
+from application.events import views
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
 
 try:
     db.create_all()
-    user_datastore.find_or_create_role('admin')
-    user_datastore.find_or_create_role('enduser')
-    user_datastore.commit()
 except:
     pass
