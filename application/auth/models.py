67e7cf8eb472a0d745b358ac53b22860d5dbce60
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-08-24 12:33:13 +0300

    adjust Role model

--- application/auth/models.py
+++ application/auth/models.py
@@ -7,7 +7,7 @@ roles_users = db.Table('roles_users',
 
 class Role(db.Model, RoleMixin):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(80), unique=True)
+    name = db.Column(db.String(64), unique=True)
     description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
