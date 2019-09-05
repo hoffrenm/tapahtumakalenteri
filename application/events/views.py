dac259b958096ba0c4e49aab3f5235b1fcc9254f
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-09-05 15:33:43 +0300

    show comment edit and remove buttons for user that has posted them

--- application/events/views.py
+++ application/events/views.py
@@ -7,17 +7,15 @@ from application.comments.forms import CommentForm
 from application.auth.models import User
 
 from flask import render_template, request, redirect, url_for, flash
-# from flask_login import current_user
 from flask_security import roles_accepted, roles_required, login_required, current_user
 
 from datetime import datetime
-
 
 @app.route("/events/show/<event_id>", methods=["GET"])
 @login_required
 def event_show(event_id):
     event = Event.query.get(event_id)
-    comments = Event.find_comments_for_event(event_id)
+    comments = Comment.find_comments_for_event(event_id)
     attendees = Event.participant_count(event_id)
     account = User.query.get(current_user.id)
 
@@ -28,12 +26,12 @@ def event_show(event_id):
     event.date = datetime.strftime(event.date_time, '%d.%m.%Y')
 
     if event in account.attending:
-        joined = True
+        userHasjoined = True
     else:
-        joined = False
+        userHasjoined = False
 
     return render_template("events/event.html", event = event, attendees=attendees, 
-                            comments=comments, form=CommentForm(), joined=joined)
+                            comments=comments, form=CommentForm(), userHasjoined=userHasjoined)
 
 @app.route("/events/join/<event_id>", methods=["POST"])
 @login_required
@@ -79,7 +77,7 @@ def event_delete(event_id):
     db.session().delete(event)
     db.session().commit()
 
-    return redirect(url_for("events_all"))
+    return redirect(url_for("admin_list"))
 
 @app.route("/events/", methods=["POST"])
 @login_required
@@ -112,7 +110,6 @@ def event_edit(event_id):
     event.date = event.date_time.strftime('%Y-%m-%d')
 
     return render_template("events/modify.html", form = EventModifyForm(), event = event)
-
 
 @app.route("/events/modify/<int:event_id>", methods=["POST"])
 @login_required
@@ -165,13 +162,12 @@ def admin_list():
 
     return render_template("events/adminlist.html", events = events)
 
-
 @app.route("/events/details/<event_id>", methods=["GET"])
 @login_required
 @roles_required('admin')
 def admin_show(event_id):
     event = Event.query.get(event_id)
-    comments = Event.find_comments_for_event(event_id)
+    comments = Comment.find_comments_for_event(event_id)
     participants = Event.find_participants_for_event(event_id)
 
     return render_template("events/details.html", event = event, comments=comments, 
