a3a919356287a86ddba972210ae079a15c5ac3c8
Mika Hoffren <mika.hoffren@helsinki.fi>
2019-09-06 16:58:05 +0300

    add queries for rolecount and users with comments and participations

--- application/auth/models.py
+++ application/auth/models.py
@@ -1,5 +1,7 @@
 from application import db
+
 from flask_security import RoleMixin, UserMixin
+from sqlalchemy.sql import text
 
 roles_users = db.Table('roles_users',
     db.Column('account_id', db.Integer(), db.ForeignKey('account.id')),
@@ -41,4 +43,36 @@ class User(db.Model, UserMixin):
 
     def is_authenticated(self):
         return True
+
+    # queries for all users and counts their comments and participations to events
+    def find_users_comments_and_participations():
+        stmt = text("SELECT Account.name, COUNT(DISTINCT Comment.id), COUNT(DISTINCT Participation.event_id)"
+                    " FROM Account"
+                    " LEFT JOIN Comment ON Comment.account_id = Account.id"
+                    " LEFT JOIN Participation ON Participation.account_id = Account.id"
+                    " GROUP BY Account.name")
+
+        res = db.engine.execute(stmt)
+
+        response = []
+
+        for row in res:
+            response.append({"name":row[0], "commentCount":row[1], "eventCount":row[2]})
+
+        return response
+
+    def find_roles_count():
+        stmt = text("SELECT Role.name, COUNT(Roles_users.account_id)"
+                    " FROM Role"
+                    " LEFT JOIN Roles_users ON Roles_users.role_id = Role.id"
+                    " GROUP BY Role.name")
+
+        res = db.engine.execute(stmt)
+
+        response = []
+
+        for row in res:
+            response.append({"name":row[0], "roleCount":row[1] })
+
+        return response
     
